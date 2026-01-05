e1b6637c7a4707587810b798bb6b8e297b9ac757
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-05 15:26:44 +0530

    Bug fix

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -5,12 +5,12 @@ const messageSchema = new mongoose.Schema(
   {
     senderId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Auth",
       required: true,
     },
     receiverId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Auth",
       required: true,
     },
     message: {
