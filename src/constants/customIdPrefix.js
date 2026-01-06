fd7a0ff9c031338703b922089fc73d66e3dea909
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-01-06 12:03:21 +0530

    Create customIdPrefix.js

---
+++ src/constants/customIdPrefix.js
@@ -0,0 +1,11 @@
+export const customIdPrefix = {
+  account: "ACT",
+  activeDevice: "ATD",
+  category: "CTG",
+  note: "NTE",
+  order: "ORD",
+  product: "PDC",
+  setting: "STG",
+  type: "TYP",
+  property: "PRP",
+};
